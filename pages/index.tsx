4a7aa1b71a2c9f76f3a47e5a1bb154c84337ad03
jonas <jonasdeprund@gmail.com>
2022-02-24 16:22:42 +0100

    playground added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,11 @@
+import { useEffect } from 'react';
+import play from '../playground';
+
 export default function Home() {
+  useEffect(() => {
+    play();
+  }, []);
+
   const message: string = 'Bonjour';
   return <div>{message}</div>;
 }
